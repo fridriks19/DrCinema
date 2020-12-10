09b63d5e246e6acb697e9ccc8fea531b887a9bc2
fridrik <fridriks19@ruis>
2020-12-10 19:20:27 +0000

    sort upcoming movies

--- src/services/upcomingMoviesService.jsx
+++ src/services/upcomingMoviesService.jsx
@@ -4,7 +4,7 @@ const upcomingMoviesService = () => {
         getUpcomingMovies: () => fetch(ENDPOINT, {
           headers: {
             'x-access-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1ZmNlMTdkNDE2ZjczZjE2NjQ4OThjNjIiLCJnbG9iYWxhZG1pbiI6ZmFsc2UsImFkbWluIjpmYWxzZSwiYWN0aXZlIjp0cnVlLCJmdWxsbmFtZSI6IkZyaWRyaWsgU3RlaW5ncmltc3MiLCJlbWFpbCI6ImZyaWRyaWsyMDAwQGdtYWlsLmNvbSIsInVzZXJuYW1lIjoiZnJlZWRyaWNrIiwicGFzc3dvcmQiOiIkMmEkMDgkZjZRZzlQa2tjYzZlMG5Xb1ZzTWZvZURBL2cubzR0ZDJjLzU1MXdNdU9FbDdWeExyN3ExQ2EiLCJkb21haW4iOiJGcmlkcnVuMTIzIiwibWVzc2FnZSI6ImZvciBzY2hvb2wgYXNzaWdubWVudCBpbiBIUiIsImlhdCI6MTYwNzYxNjUwMywiZXhwIjoxNjA3NzAyOTAzfQ.t-IycxpRWxxIHl8nG40CdifnoRMlURW2hHoA4RytLjY'}})
-        .then(response => response.json()) //.then(response => response.sort((a, b) => { return a.release-dateIS> b.release-dateIS  }))
+        .then(response => response.json()).then(response => response.sort((a, b) => { return a["release-dateIS"] < b["release-dateIS"]  }))
     };
 };
 
